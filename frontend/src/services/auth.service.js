42018b7bc91a0c4ba4727829addd54e8e8572e02
coder-nitin07 <nitinthakur87504@gmail.com>
2026-06-27 18:32:23 +0530

    create auth service file for auth modules

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,27 @@
+import api from "@/lib/axios";
+
+const register = async (userData) =>{
+    const response  = await api.post('/auth/register', userData);
+
+    return response.data;
+};
+
+const login = async (userData) =>{
+    const response = await api.post('/auth/login', userData);
+
+    return response.data;
+};
+
+const logout = async () =>{
+    const response = await api.post('/auth/logout');
+
+    return response.data;
+};
+
+const refreshToken = async () =>{
+    const response = await api.post('/auth/refresh-token');
+
+    return response.data;
+};
+
+export { register, login, logout, refreshToken };
